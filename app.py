4de5278dbd803fd0ccd9c396310769e158e25f0f
yuchernyshov@usurt.ru <ychernyshov@localhost.localdomain>
2021-07-14 07:36:13 -0400

    Добавлена функция predict с линейной регрессией

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import sqlite3
+from sklearn.linear_model import LinearRegression
+from sys import argv
 
 
 app = Flask(__name__)
@@ -10,16 +12,12 @@ def hello():
     return render_template("index.html")
 
 
-
 @app.route("/all")
 def all():
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
-
     customers = c.execute("SELECT * from customer").fetchall()
-
     conn.close()
-
     return render_template("all.html", customers=customers)
 
 
@@ -27,11 +25,8 @@ def all():
 def one(number):
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
-
     customer = c.execute("SELECT * from customer WHERE number=?", (number,)).fetchone()
-
     conn.close()
-
     return render_template("all.html", customers=(customer,))
 
 
@@ -40,20 +35,14 @@ def create():
     if request.method == 'POST':
         conn = sqlite3.connect("database.db")
         c = conn.cursor()
-
         number = request.form['number']
         surname = request.form['surname']
-
         c.execute("INSERT INTO customer VALUES(?,?)", (number, surname))
-
         conn.commit()
-
         conn.close()
-
         return render_template("index.html")
 
     else:
-
         return render_template("create.html")
 
 
@@ -61,12 +50,22 @@ def create():
 def update(number, surname):
     conn = sqlite3.connect("database.db")
     c = conn.cursor()
-
     c.execute("UPDATE customer SET surname=? WHERE number=?", (surname, number))
-
     conn.commit()
-
     conn.close()
-
     return render_template("index.html")
+
+
+@app.route("/predict/<int:data>")
+def predict(data):
+    model = LinearRegression()
+    xs = [[1], [2], [3]]
+    ys = [1, 2, 3]
+    model.fit(xs, ys)
+    result = model.predict([[data]])
+    return jsonify({"prediction": list(result)})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=argv[1], debug=True)
 
